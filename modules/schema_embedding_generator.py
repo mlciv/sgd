9f57a7c6df6b6a24bc1d13b21889c0d82706e954
Jie.Cao <cajie@amazon.com>
2020-10-01 17:08:56 -0700

    fix embedding

--- modules/schema_embedding_generator.py
+++ modules/schema_embedding_generator.py
@@ -897,14 +897,14 @@ class SchemaEmbeddingGenerator(nn.Module):
                 logger.info("Generating schema feature for service %s.", service)
                 completed_services.add(service)
             schema_type = feature.schema_type
-            if schema_type.endswith("_value_seq2")
+            if schema_type.endswith("_value_seq2"):
                 input_ids_mat = schema_features[feature.service_id][feature.input_ids_tensor_name]
                 input_mask_mat = schema_features[feature.service_id][feature.input_mask_tensor_name]
                 input_type_ids_mat = schema_features[feature.service_id][feature.input_type_ids_tensor_name]
                 input_ids_mat[feature.intent_or_slot_id, feature.value_id] = feature.input_ids
                 input_mask_mat[feature.intent_or_slot_id, feature.value_id] = feature.input_mask
                 input_type_ids_mat[feature.intent_or_slot_id, feature.value_id] = feature.input_type_ids
-            elif schema_type.endswith("_value_seq")
+            elif schema_type.endswith("_value_seq"):
                 # precomputing the cls
                 emb_mat = schema_features[feature.service_id][feature.embedding_tensor_name]
                 if self.enc_model_type in ["xlm", "roberta", "distilbert", "camembert"]:
