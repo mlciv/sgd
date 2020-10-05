3122cf233f7103f7dd3db6c3041025b98c9fc0c6
Jie.Cao <cajie@amazon.com>
2020-10-05 14:14:38 -0700

    add request fusion

--- modules/modelling_dstc8baseline.py
+++ modules/modelling_dstc8baseline.py
@@ -42,26 +42,6 @@ class DSTC8BaselineModel(PreTrainedModel, DSTC8BaselineOutputInterface):
 
     config_class = SchemaDSTConfig
     base_model_prefix = ""
-    # see the trick in https://github.com/huggingface/transformers/blob/cae641ff269478f74b5895d36fbc686f7074f2fb/transformers/modeling_utils.py#L457
-    # 1.To support the base model part from a model
-    # 1.1. should only load the basemodel part of a different deveried model
-    # 1.2. the original BertModel has base_model_prefix="", and no attr called base_model_prefix; A derived model will have a base_model_prefix="bert/roberta", and have parameters started with $base_model_prefix
-    # Assumption 1: A derived model has an attr called $bas_model_prefix$, also its state_dict has parameter started with $base_model_prefix$. Then it will not meet any of the condition in the code. staret_prefix and model_to_load will not change. But if the source model is not eaxactly the same as the target model, some parameters will show warning.
-    # Assumption 2: A base model has no attr or parameters started with base_model_str.
-    # 2. When loading, we will check base the target model and source state_dict
-    # if the model has attribute called $base_model_prefix$, but state_dict has no $base_model_prefix$(the base mode itself). Then we only load the base model part.
-    # 2.1. Target: A derived model, Source: A derived model
-    # condition 1 and 2 will failed. no change to source and target
-    # 2.2  Target: A derived model, Source: base model part.
-    # condition 1 will fail: all state_dict will be used, no prefix will change
-    # condition 2 will succeed. only the base_model part in the target model will be fill
-    # 2.3  Target: A based model. Source: A base model
-    # condition 1 will fail: all state_dict will be used, no prefix will change
-    # condition 2 will fail. all target model will be filled
-    # 2.4 Target: Base model. Source: Derived model
-    # condition 1 will succeed, only the basemodel part of source derived model  will be used
-    # condition 2 will fail. No change for the source model part.
-
 
     pretrained_model_archieve_map = CLS_PRETRAINED_MODEL_ARCHIVE_MAP
 
