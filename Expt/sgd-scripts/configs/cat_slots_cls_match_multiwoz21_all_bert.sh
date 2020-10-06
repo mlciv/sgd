b6e5b5f84cac809184e57bbd3da947b4bd7fd1a2
Jie.Cao <cajie@amazon.com>
2020-10-05 23:50:16 -0700

    add script

--- Expt/sgd-scripts/configs/cat_slots_cls_match_multiwoz21_all_bert.sh
+++ Expt/sgd-scripts/configs/cat_slots_cls_match_multiwoz21_all_bert.sh
@@ -3,20 +3,20 @@ config_name=`basename "$1"`
 
 EXP_NAME="${config_name%.*}"
 echo $EXP_NAME
-TASK_NAME=dstc8_all
+TASK_NAME=multiwoz21_all
 EXP_DIR=$SGD_WORK_DIR/$TASK_NAME/$EXP_NAME/
 EXP_MODELS=$EXP_DIR/models/
 EXP_SUMMARY=$EXP_DIR/summary/
 EXP_RESULTS=$EXP_DIR/results/
 
 # model_type, some model name to initialize or load pretrained model
-MODEL_TYPE=requested_slots_cls_match
+MODEL_TYPE=cat_slots_cls_match
 # encoder config name for the task
 ENCODER_CONFIG_NAME=
 # encoder_model_name_path, whether a name or a path for the model
 ENCODER_MODEL_NAME_PATH=$SGD_JSON_CONFIG_DIR/encoders/bert-base-cased.json
 # config name for the task
-CONFIG_NAME=$SGD_JSON_CONFIG_DIR/models/requested_slots_cls_match.json
+CONFIG_NAME=$SGD_JSON_CONFIG_DIR/models/cat_slots_cls_match.json
 # model_name_path, whether a name or a path for the model
 MODEL_NAME_PATH=
 # cache_dir, the cache_dir for store the mebdding, exampls.
@@ -40,7 +40,7 @@ LEARNING_RATE=1e-4
 # gradient_accumulation_steps
 GRADIENT_ACCUMULATION_STEPS=2
 # logging_steps
-LOGGING_STEPS=4000
+LOGGING_STEPS=3000
 # save_steps 
 SAVE_STEPS=1000000
 # JOINT_ACC_ACROSS_TURN
